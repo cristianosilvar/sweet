7ce0092fa794f66bf903a821b6f3c11954a55415
“cristianosilvar” <cristianosilva2k@hotmail.com>
2024-02-24 15:14:00 -0300

    feat: add home, video & category in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from '../pages/home'
+import Video from '../pages/video'
+import Category from '../pages/category'
+
 import ProtectedRoute from './protectedRoute'
 
 const RoutesComponent = () => {
@@ -6,9 +11,9 @@ const RoutesComponent = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ProtectedRoute />}>
-          <Route index element={<ProtectedRoute />} />
-          <Route path="video/:id" element={<ProtectedRoute />} />
-          <Route path="category/:id" element={<ProtectedRoute />} />
+          <Route index element={<Home />} />
+          <Route path="video/:id" element={<Video />} />
+          <Route path="category/:id" element={<Category />} />
         </Route>
         <Route path="*" element={<p>Page not found</p>} />
       </Routes>
