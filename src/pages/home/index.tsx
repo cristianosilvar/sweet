c6d0d95c573c8dc9150d6c0d8e014e1b474f23b0
“cristianosilvar” <cristianosilva2k@hotmail.com>
2024-03-10 20:46:47 -0300

    wip: player preview

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,21 @@
-import ButtonLink from '../../components/ui/buttonLink'
+import ReactPlayer from 'react-player'
 
-const Home = () => {
+import ButtonLink from '../../components/ui/buttonLink'
+import { useState } from 'react'
+
+function Home() {
+  const [isPlaying, setPlaying] = useState(false)
+
   return (
     <div className="px-2">
       <ButtonLink textButton="sports" />
+      <div
+        onMouseOver={() => setPlaying(true)}
+        onMouseLeave={() => setPlaying(false)}
+        className="w-min overflow-hidden rounded-lg"
+      >
+        <ReactPlayer url="https://vimeo.com/354218747" playing={isPlaying} />
+      </div>
     </div>
   )
 }
